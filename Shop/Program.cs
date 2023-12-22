394c2f3788a31ecb0cc2d35b7f5039725923d907
Martin1337 <115101060+MarWalldorf@users.noreply.github.com>
2023-12-22 14:27:48 +0100

    Add files via upload

---
+++ Shop/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome();
+            PurchaseItem();
+        }
+
+        static void Welcome()
+        {
+            Console.WriteLine("Welcome to the store! Here are the items that you can buy:");
+            Console.WriteLine("Book");
+            Console.WriteLine("Shirt");
+            Console.WriteLine("Computer");
+            Console.WriteLine("What would you like to buy?");
+        }
+
+        static void PurchaseItem()
+        {
+            var item = Console.ReadLine();
+
+            switch (item)
+            {
+                case "Book":
+                    var book = new Book();
+                    book.Author = "JK Author";
+                    book.Title = "The wizard of coding";
+                    book.Price = 9.99;
+                    Console.WriteLine($"You purchase a {book.Title} by" + $" {book.Author} for {book.Price}");
+                    break;
+                case "Shirt":
+                    var shirt = new Shirt();
+                    shirt.Size = "M";
+                    shirt.Title = "Sports Shirt";
+                    shirt.Price = 14.99;
+                    Console.WriteLine($"You purchase a {shirt.Title} sized" + $" {shirt.Size} for {shirt.Price}");
+                    break;
+                case "Computer":
+                    var computer = new Computer();
+                    computer.Type = "PC";
+                    computer.Title = "Surface Book Pro";
+                    computer.Price = 1499.99;
+                    Console.WriteLine($"You purchase a {computer.Title}" + $" {computer.Type} for {computer.Price}");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    class BaseItem
+    {
+        public string Title { get; set; }
+        public double Price { get; set; }
+    }
+
+    class Book : BaseItem
+    {
+        public string Author { get; set; }
+    }
+
+    class Shirt : BaseItem
+    {
+        public string Size { get; set; }
+    }
+
+    class Computer : BaseItem
+    {
+        public string Type { get; set; }
+    }
+}
